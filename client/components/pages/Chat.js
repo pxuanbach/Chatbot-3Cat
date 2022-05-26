f94288d8c41587048d0804ad7891c1c52c66c422
pxuanbach <pxuanbach1412@gmail.com>
2022-05-26 16:49:31 +0700

    Go back to the sign up screen
    go back to the first screen of screens deep in a stack

--- client/components/pages/Chat.js
+++ client/components/pages/Chat.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-const Chat = () => {
+const Chat = ({navigation}) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Chat Screen</Text>
+      <Button
+        title="Go Back"
+        onPress={() => navigation.popToTop()}
+      ></Button>
     </View>
 
   )
