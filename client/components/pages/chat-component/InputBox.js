b7f664f1af7452df623052943cdaf7953443fe2a
pxuanbach <pxuanbach1412@gmail.com>
2022-07-04 00:08:01 +0700

    Update InputBox.js

--- client/components/pages/chat-component/InputBox.js
+++ client/components/pages/chat-component/InputBox.js
@@ -8,27 +8,6 @@ import { Octicons } from '@expo/vector-icons';
 import { SimpleLineIcons } from '@expo/vector-icons';
 import axiosInstance from '../../../AxiosInstance';
 
-const recordingOptions = {
-  android: {
-      extension: '.m4a',
-      outputFormat: Audio.RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_MPEG_4,
-      audioEncoder: Audio.RECORDING_OPTION_ANDROID_AUDIO_ENCODER_AAC,
-      sampleRate: 44100,
-      numberOfChannels: 1,
-      bitRate: 128000,
-  },
-  ios: {
-      extension: '.wav',
-      audioQuality: Audio.RECORDING_OPTION_IOS_AUDIO_QUALITY_HIGH,
-      sampleRate: 44100,
-      numberOfChannels: 1,
-      bitRate: 128000,
-      linearPCMBitDepth: 16,
-      linearPCMIsBigEndian: false,
-      linearPCMIsFloat: false,
-  },
-}
-
 const InputBox = ({ user, setMessages }) => {
   const [message, setMessage] = useState('');
   const [recording, setRecording] = useState();
