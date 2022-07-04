3711bec60dc7de425079aa51789be4e82322f62a
Phạm Xuân Bách <55500268+pxuanbach@users.noreply.github.com>
2022-07-04 20:17:18 +0700

    Adjust router

--- client/AxiosInstance.js
+++ client/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://9541-2402-800-6215-af8e-68b3-b8a6-3154-e2ff.ap.ngrok.io",
+  baseURL: "https://chatbot-3cat.herokuapp.com/",
 })
 
 export default axiosInstance
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,9 @@
 const {Router} = require('express');
 const authController = require('../controllers/authControllers');
 const router = Router();
-
+router.get('/', (req, res) => {
+    res.send({message: "Hello world"})
+})
 router.post('/signup', authController.signup)
 router.post('/login', authController.login)
 router.get('/logout', authController.logout)
